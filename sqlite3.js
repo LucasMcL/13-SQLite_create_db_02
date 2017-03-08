eef93de7b332350ad9107ec6f1b84ebd68c36ac0
LucasMcL <LMcLaughlin09@gmail.com>
2017-03-08 14:12:06 -0600

    completed assignment

--- sqlite3.js
+++ sqlite3.js
@@ -4,14 +4,58 @@ const sqlite3 = require('sqlite3')
 const db = new sqlite3.Database('business_records.sqlite')
 
 // Create emloyees table
-db.run(`Create table if not exists employees
-	(id INTEGER PRIMARY KEY,
-	 firstName TEXT,
-	 lastName TEXT,
-	 jobTitle TEXT,
-	 address TEXT
-	)`)
+const createTable = function() {
+	db.run(`Create table if not exists employees
+		(id INTEGER PRIMARY KEY,
+		 firstName TEXT,
+		 lastName TEXT,
+		 jobTitle TEXT,
+		 address TEXT
+		)`)
+}
+// createTable()
 
-const data = require('./employees.json')
+const populateTable = function() {
+	// Get employees data from .json file
+	const {employees} = require('./employees.json')
 
-db.run()
+	employees.forEach(employee => {
+		const {id, firstName, lastName, jobTitle, address} = employee
+		db.run(`INSERT INTO employees (id, firstName, lastName, jobTitle, address) VALUES
+						(${id}, "${firstName}", "${lastName}", "${jobTitle}", "${address}")`, err => {if(err) console.error(err)})
+	})
+}
+// populateTable()
+
+const logEmployeeRecords = function() {
+	db.all(`SELECT * FROM employees`, (err, data) => {
+		if(err) console.error(err)
+		data.forEach(obj => console.log(obj))
+	})
+}
+// logEmployeeRecords()
+
+const logEmployeeJobTitles = function() {
+	db.all(`SELECT jobTitle FROM employees`, (err, data) => {
+		if(err) console.error(err)
+		data.forEach(obj => console.log(obj))
+	})
+}
+// logEmployeeJobTitles()
+
+const logEmployeeAddresses = function() {
+	db.all(`SELECT firstName, lastName, address FROM employees`, (err, data) => {
+		if(err) console.error(err)
+		data.forEach(obj => console.log(obj))
+	})
+}
+// logEmployeeAddresses()
+
+
+
+
+
+
+
+
+
